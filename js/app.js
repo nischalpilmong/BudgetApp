a8fa375335e05323bd36ed46be057bd197ea6ba1
Nischal Rai <nischalpilmong@yahoo.com>
2020-04-15 00:22:32 +0200

    added new Item to the UI

--- js/app.js
+++ js/app.js
@@ -56,7 +56,9 @@ var UIController = (function(){
            inputType: '.add__type',
            inputDescription: '.add__description',
            inputValue: '.add__value',
-           inputBtn: '.add__btn'
+           inputBtn: '.add__btn',
+           incomeContainer: '.income__list',
+           expenseContainer: '.expenses__list'
      };
     return {
       getInput: function(){
@@ -65,6 +67,25 @@ var UIController = (function(){
           description: document.querySelector(DOMstrings.inputDescription).value,
            value: document.querySelector(DOMstrings.inputValue).value
          };         
+      },
+      addListItem: function(obj,type){
+        var html, newHtml, element;
+        //Create HTML string with placeholder text
+        if(type === 'inc'){
+          element = DOMstrings.incomeContainer;
+          html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+        }
+        else if(type === 'exp'){
+          element = DOMstrings.expenseContainer;
+          html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+        }
+        //Replace the placeholder text with some actual data
+        newHtml = html.replace('%id%',obj.id);
+        newHtml = newHtml.replace('%description%',obj.description);
+        newHtml = newHtml.replace('%value%', obj.value);
+
+        //Insert the HTML into DOM
+        document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
       },
       //Making DOMStrings object from one module(controller) to another
       //making it public by creating function inside return { }
@@ -98,6 +119,7 @@ var appController = (function(budgetCtrl, UICtrl) {
        //2.Add the item to the budget Controller
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
        //3.Add the item to the UI
+       UICtrl.addListItem(newItem, input.type);
        //4.calculate the budget
        //5.display the budget
     };
