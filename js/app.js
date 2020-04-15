fdd6d0df3076ef6405895e3e28ab4451745de602
Nischal Rai <nischalpilmong@yahoo.com>
2020-04-15 13:24:55 +0200

    displayed the budget,income and expense in UI

--- js/app.js
+++ js/app.js
@@ -10,6 +10,11 @@ var budgetController = (function() {
        this.description = description;
        this.value = value;
     };
+    var calculateTotal = function(type){
+       var sum = 0;
+       data.allItems[type].forEach((cur) => sum += cur.value );
+       data.totals[type] = sum;
+    };
     var data = {
          allItems: {
              exp: [],
@@ -18,7 +23,9 @@ var budgetController = (function() {
          totals: {
            exp: 0,
            inc: 0
-         }
+         },
+         budget: 0,
+         percentage: -1 //-1 refers non-exist value when there is no budget or no income and expenses
     };
     return {
       addItem: function(type, des, val){
@@ -44,6 +51,30 @@ var budgetController = (function() {
         //Return new Element
         return newItem;
       },
+      calculateBudget: function(){
+            //calculate total incomes and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+            //calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+            //calculate the percentage of income that we spent
+            if(data.totals.inc > 0){    //income should be greater than 0 otherwise the percentage value will be infinite.
+              data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            }
+            else{
+              data.percentage = -1;
+            }
+            
+          
+      },
+      getBudget: function(){
+            return {
+                 budget: data.budget,
+                 totalInc: data.totals.inc,
+                 totalExp: data.totals.exp,
+                 percentage: data.percentage
+            };
+      },
       testing: function(){
          console.log(data);
       }
@@ -58,7 +89,12 @@ var UIController = (function(){
            inputValue: '.add__value',
            inputBtn: '.add__btn',
            incomeContainer: '.income__list',
-           expenseContainer: '.expenses__list'
+           expenseContainer: '.expenses__list',
+           budgetLabel: '.budget__value',
+           incomeLabel: '.budget__income--value',
+           expensesLabel: '.budget__expenses--value',
+           percentageLabel: '.budget__expenses--percentage'
+
      };
     return {
       getInput: function(){
@@ -96,6 +132,18 @@ var UIController = (function(){
          fieldsArr.forEach((current, index, array) => current.value = '';)
         */
       },
+      displayBudget: function(obj){
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+            
+            if(obj.percentage > 0){
+              document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            }
+            else{
+              document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+            }
+      },
       //Making DOMStrings object from one module(controller) to another
       //making it public by creating function inside return { }
       getDOMstrings: function(){
@@ -121,8 +169,11 @@ var appController = (function(budgetCtrl, UICtrl) {
     };
     var updateBudget = function(){
       //1.calculate the budget
-       //2.display the budget
+       budgetCtrl.calculateBudget();
+       //2.Return the budget
+        var budget = budgetCtrl.getBudget();
        //3.Display the budget in UI
+       UICtrl.displayBudget(budget);
     };
     
     var ctrlAddItem = function(){
@@ -144,6 +195,13 @@ var appController = (function(budgetCtrl, UICtrl) {
     };
     return {
       init: function(){
+        console.log('Application has started');
+           UICtrl.displayBudget({
+             budget: 0,
+             totalInc: 0,
+             totalExp: 0,
+             percentage: -1
+           });
            setupEventListeners();
            }
          };
